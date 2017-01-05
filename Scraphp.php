0b4308d0fa8f7081790ce17f0e470347105ab55f
Terry Lin <terrylinooo@users.noreply.github.com>
2017-01-05 18:29:00 +0800

    Empty class
    Will be updated with complete code soon. (When I think it works perfectly)

---
+++ Scraphp.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Scraphp;
+
+/*
+ *
+ * Licensed under The GNU GENERAL PUBLIC LICENSE V3
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author Terry Lin <terrylin.developer@gmail.com>
+ * @version 1.0 ($Rev: 1 $)
+ */
+ 
+use Scraphp\ScraphpCore;
+
+class Scraphp extends ScraphpCore
+{
+
+}
